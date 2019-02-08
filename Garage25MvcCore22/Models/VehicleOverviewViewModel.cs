d672126179b2c7f1f065784c444d9ec65d01519d
Håkan Kilström <hakan.kilstrom@outlook.com>
2019-02-08 11:16:02 +0100

    SMall changes in master

--- Garage25MvcCore22/Models/VehicleOverviewViewModel.cs
+++ Garage25MvcCore22/Models/VehicleOverviewViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace Garage25MvcCore22.Models
         public VehicleType VehicleType { get; set; }
         public string RegNr { get; set; }
         public int VehicleId { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime StartTime { get; set; }
         //public TimeSpan Duration { get; set; }
     }
